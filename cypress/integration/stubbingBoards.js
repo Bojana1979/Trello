605f60094939b58421742a87df4e3ed0104d7e50
Bojana1979 <bojana_jakovljevic@yahoo.com>
2021-10-21 04:03:02 -0700

    My third commit

--- cypress/integration/stubbingBoards.js
+++ cypress/integration/stubbingBoards.js
@@ -1,23 +1,38 @@
 ///<reference types = "cypress"/>
+import liste from "../fixtures/threeLists.json";
+import boards from "../fixtures/fourBoards.json";
 
 describe("Stubbing", () => {
     it("Boards stubbing", () => {
-        cy.intercept('/api/boards',{fixture : 'fourBoards'}).as('stubbedBoards')
+        cy.intercept('/api/boards',{fixture : 'fourBoards.json'}).as('stubbedBoards')
         cy.visit("/")
         cy.get('@stubbedBoards').its('response').then((res) => {
             console.log(res);
-            expect(res.body.boards[0].name).to.eq("Todo")
-            expect(res.body.boards[0].starred).to.eq(false)
+            expect(res.body[0].name).to.eq(boards[0].name)
+            expect(res.body[0].starred).to.eq(false)
             expect(res.statusCode).to.eq(200)
-            expect(res.body.boards[1].name).to.eq("In progress")
-            expect(res.body.boards[1].starred).to.eq(true)
-            expect(res.body.boards[2].name).to.eq("In QA")
-            expect(res.body.boards[2].starred).to.eq(false)
-            expect(res.body.boards[3].name).to.eq("Done")
-            expect(res.body.boards[3].starred).to.eq(false)
-            expect(res.body.lists[0].title).to.eq("Stubbing network responses")
-            expect(res.body.lists[1].title).to.eq("Changing parts of response data")
-            expect(res.body.lists[2].title).to.eq("Intercepting")
+            expect(res.body[1].name).to.eq(boards[1].name)
+            expect(res.body[1].starred).to.eq(true)
+            expect(res.body[2].name).to.eq(boards[2].name)
+            expect(res.body[2].starred).to.eq(false)
+            expect(res.body[3].name).to.eq(boards[3].name)
+            expect(res.body[3].starred).to.eq(false)
+        })
+    });
+    it("OA board stubbing", () => {
+        cy.intercept('/api/boards',{fixture : 'threeLists.json'}).as('stubbedQa')
+        cy.visit("/")
+        cy.get('@stubbedQa').its('response').then((res) => {
+            console.log(res);
+            expect(res.body[0].name).to.eq(liste[0].name)
+            expect(res.body[0].id).to.eq(65810796973)
+            expect(res.body[0].starred).to.eq(false)
+            expect(res.body[0].lists[0].title).to.eq(liste[0].lists[0].title)
+            expect(res.body[0].lists[0].boardId).to.eq(65810796973)
+            expect(res.body[0].lists[1].title).to.eq(liste[0].lists[1].title)
+            expect(res.body[0].lists[1].boardId).to.eq(65810796973)
+            expect(res.body[0].lists[2].title).to.eq(liste[0].lists[2].title)
+            expect(res.body[0].lists[2].boardId).to.eq(65810796973)
         })
     })
 })
