4b1319ab22928e525b5d0841672c346f59573187
Bojana1979 <bojana_jakovljevic@yahoo.com>
2021-10-22 01:45:19 -0700

    my fourth commit

--- cypress/integration/stubbingBoards.js
+++ cypress/integration/stubbingBoards.js
@@ -1,6 +1,7 @@
 ///<reference types = "cypress"/>
 import liste from "../fixtures/threeLists.json";
 import boards from "../fixtures/fourBoards.json";
+import tasks from "../fixtures/fourTasks.json";
 
 describe("Stubbing", () => {
     it("Boards stubbing", () => {
@@ -33,6 +34,31 @@ describe("Stubbing", () => {
             expect(res.body[0].lists[1].boardId).to.eq(65810796973)
             expect(res.body[0].lists[2].title).to.eq(liste[0].lists[2].title)
             expect(res.body[0].lists[2].boardId).to.eq(65810796973)
+        })
+    });
+    it("tasks in QA boards stubbing", () => {
+        cy.intercept('/api/boards', {fixture : 'fourTasks.json'}).as('stubbedTasks')
+        cy.visit("/")
+        cy.get('@stubbedTasks').its('response').then((res) => {
+            console.log(res)
+            expect(res.statusCode).to.eq(200)
+            expect(res.body.id).to.eq(tasks.id)
+            expect(res.body.name).to.eq(tasks.name)
+            expect(res.body.lists[0].title).to.eq(tasks.lists[0].title)
+            expect(res.body.lists[1].title).to.eq(tasks.lists[1].title)
+            expect(res.body.lists[2].title).to.eq(tasks.lists[2].title)
+            expect(res.body.tasks[0].title).to.eq(tasks.tasks[0].title)
+            expect(res.body.tasks[0].boardId).to.eq(tasks.id)
+            expect(res.body.tasks[0].listId).to.eq(tasks.lists[0].id)
+            expect(res.body.tasks[1].title).to.eq(tasks.tasks[1].title)
+            expect(res.body.tasks[1].boardId).to.eq(tasks.id)
+            expect(res.body.tasks[1].listId).to.eq(tasks.lists[0].id)
+            expect(res.body.tasks[2].title).to.eq(tasks.tasks[2].title)
+            expect(res.body.tasks[2].boardId).to.eq(tasks.id)
+            expect(res.body.tasks[2].listId).to.eq(tasks.lists[0].id)
+            expect(res.body.tasks[3].title).to.eq(tasks.tasks[3].title)
+            expect(res.body.tasks[3].boardId).to.eq(tasks.id)
+            expect(res.body.tasks[3].listId).to.eq(tasks.lists[0].id)
         })
     })
 })
